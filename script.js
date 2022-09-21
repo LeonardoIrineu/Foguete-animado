d026847c72ee10f6a878e0ea6a238c1da406c95c
Leonardo Irineu <leo_izidoro_@hotmail.com>
2022-09-21 00:13:41 -0300

    correção da velocidade do foguete e resolvendo quebra de codigo no css

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-function star(){
-    let count = 50
-    let scene = document.querySelector(".scene")
-    let i=0
+function stars(){
+    let count = 150;
+    let scene = document.querySelector('.scene');
+    let i =0;
     while(i < count){
-        let star = document.createElement('i')
-        let x = Math.floor(Math.random()*window.innerWidth)
-        let duration = Math.random()*1
-        let h = Math.random()*100
+        let star = document.createElement('i');
+        let x = Math.floor(Math.random() * window.innerWidth);
+        let duration = Math.random() * 1;
+        let h = Math.random() * 100;
 
-        star.style.left = x + 'px'
-        star.style.left = 1 + 'px'
-        star.style.height = h + 'px'
-        star.style.animationDuration = duration + 's'
-
-        scene.appendChild(star)
-        i++
+        star.style.left = x + 'px';
+        star.style.width = 1 + 'px';
+        star.style.height = 200 + 'px';
+        star.style.animationDuration = duration + 's';
+        
+        scene.appendChild(star);
+        i++;
     }
 }
-star()
+stars();
